e25707687dfb821aca4b3c52a0e6b28f7713e6fb
ZenPhysics <koestline@gmail.com>
2014-02-23 19:49:04 -0600

    Changed .gitignore
    .gitIgnore contains bundler

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'f0b5086f8367502cf97a2495b82271f77adb308cc8ca20cc3879194f2b81ca15712bc7d402f8e4fce14ea6fef599be6b2bda1c44262183bd3e152725d7fda4a9'
